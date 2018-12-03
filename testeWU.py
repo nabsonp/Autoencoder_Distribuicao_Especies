59a4cfe8d09f353cf872870e3ea9749e6c126f7b
Nabson Paiva <nabsonp@gmail.com>
2018-12-03 11:42:59 -0400

    Trata solicitação sem dados e limite diário

--- testeWU.py
+++ testeWU.py
@@ -5,7 +5,7 @@ import time
 
 key = "c137e42a99ef99f0"
 base = open('americanToad.csv',"rt")
-out = open("americanToadCorre.csv","w")
+out = open("americanToadCorre.csv","a")
 #base = open('springPeeper.csv',"rt")
 #out = open("springPeeperCorre.csv","w")
 #base = open('greenFrog.csv',"rt")
@@ -14,20 +14,25 @@ reader = csv.reader(base)
 i = 0
 a = 0
 erros = []
-fieldnames = ['date','lat', 'lng', 'tornado', 'monthtodatesnowfalli', 'thunder', 'meantempi', 'snow', 'meanvism', 'meanwdire', 'minhumidity', 'maxvism', 'since1jancoolingdegreedays', 'coolingdegreedays', 'maxdewptm', 'monthtodatecoolingdegreedays', 'since1sepheatingdegreedays', 'precipi', 'heatingdegreedays', 'meanpressurem', 'since1julsnowfallm', 'monthtodatesnowfallm', 'precipsource', 'rain', 'minwspdm', 'since1sepcoolingdegreedaysnormal', 'meanwindspdi', 'monthtodateheatingdegreedaysnormal', 'snowfallm', 'fog', 'humidity', 'minwspdi', 'meanpressurei', 'gdegreedays', 'since1julheatingdegreedays', 'mindewptm', 'maxwspdi', 'minpressurem', 'coolingdegreedaysnormal', 'maxtempm', 'minvisi', 'meanwindspdm', 'mintempi', 'maxpressurem', 'since1sepheatingdegreedaysnormal', 'hail', 'meandewptm', 'maxwspdm', 'precipm', 'meantempm', 'since1julheatingdegreedaysnormal', 'snowfalli', 'monthtodateheatingdegreedays', 'monthtodatecoolingdegreedaysnormal', 'since1sepcoolingdegreedays', 'maxpressurei', 'minvism', 'minpressurei', 'maxvisi', 'heatingdegreedaysnormal', 'since1jancoolingdegreedaysnormal', 'snowdepthm', 'meanwdird', 'meandewpti', 'meanvisi', 'maxhumidity', 'snowdepthi', 'mintempm', 'since1julsnowfalli', 'maxtempi', 'maxdewpti', 'mindewpti']
+fieldnames = ['id','date','lat', 'lng', 'tornado', 'monthtodatesnowfalli', 'thunder', 'meantempi', 'snow', 'meanvism', 'meanwdire', 'minhumidity', 'maxvism', 'since1jancoolingdegreedays', 'coolingdegreedays', 'maxdewptm', 'monthtodatecoolingdegreedays', 'since1sepheatingdegreedays', 'precipi', 'heatingdegreedays', 'meanpressurem', 'since1julsnowfallm', 'monthtodatesnowfallm', 'precipsource', 'rain', 'minwspdm', 'since1sepcoolingdegreedaysnormal', 'meanwindspdi', 'monthtodateheatingdegreedaysnormal', 'snowfallm', 'fog', 'humidity', 'minwspdi', 'meanpressurei', 'gdegreedays', 'since1julheatingdegreedays', 'mindewptm', 'maxwspdi', 'minpressurem', 'coolingdegreedaysnormal', 'maxtempm', 'minvisi', 'meanwindspdm', 'mintempi', 'maxpressurem', 'since1sepheatingdegreedaysnormal', 'hail', 'meandewptm', 'maxwspdm', 'precipm', 'meantempm', 'since1julheatingdegreedaysnormal', 'snowfalli', 'monthtodateheatingdegreedays', 'monthtodatecoolingdegreedaysnormal', 'since1sepcoolingdegreedays', 'maxpressurei', 'minvism', 'minpressurei', 'maxvisi', 'heatingdegreedaysnormal', 'since1jancoolingdegreedaysnormal', 'snowdepthm', 'meanwdird', 'meandewpti', 'meanvisi', 'maxhumidity', 'snowdepthi', 'mintempm', 'since1julsnowfalli', 'maxtempi', 'maxdewpti', 'mindewpti']
 
 writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter = ',')
-writer.writeheader()
+writerError = csv.writer(out)
+#writer.writeheader()
 print("Iniciando coleta de dados...")
 for row in reader:
     while (a < 3):
         try:
             f = urllib.request.urlopen("http://api.wunderground.com/api/"+key+"/history_"+row[1]+"/q/"+row[2]+","+row[3]+".json")
             parsed_json = json.loads(f.read())
-            parsed_json['history']['dailysummary'][0]['date'] = row[1]
-            parsed_json['history']['dailysummary'][0]['lat'] = row[2]
-            parsed_json['history']['dailysummary'][0]['lng'] = row[3]
-            writer.writerow(parsed_json['history']['dailysummary'][0])
+            if (parsed_json['history']['dailysummary']):
+                parsed_json['history']['dailysummary'][0]['id'] = row[0]
+                parsed_json['history']['dailysummary'][0]['date'] = row[1]
+                parsed_json['history']['dailysummary'][0]['lat'] = row[2]
+                parsed_json['history']['dailysummary'][0]['lng'] = row[3]
+                writer.writerow(parsed_json['history']['dailysummary'][0])
+            else:
+                writerError.writerow([row[0],row[1],row[2],row[3]])
             f.close()
             a = 4
         except:
@@ -44,8 +49,12 @@ for row in reader:
         print(i,"resposta(s)")
     a = 0
     if (i % 9 == 0 and i != 0):
-        print("Aguardando 1 minuto...")
+        print("Limite de 10/min atingido. Aguardando 1 minuto...")
         time.sleep(60)
+        print("Continuando coleta de dados...")
+    elif (i % 499 == 0 and i != 0):
+        print("Limite de 500/dia atingido. Aguardando 23 horas...")
+        time.sleep(72000)
         print("Continuando coleta de dados...")
 
 out.close()
